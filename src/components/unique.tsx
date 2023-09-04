d4e44a7e1f6e70b56a91a5f10760c71879bd43d7
bub655 <52846238+bub655@users.noreply.github.com>
2023-09-03 17:24:42 -0700

    hero and unique - top two components

---
+++ src/components/unique.tsx
@@ -0,0 +1,27 @@
+export const Unique = () => {
+  return (
+    <div className="m-12 mt-20 flex flex-wrap justify-between">
+      <div className="w-6/12 ml-6">
+        <div className="text-4xl">
+          <span className="font-black shadow-purple-400">Solve problems for  </span>
+          <span className="font-black drop-shadow-[3px_4px_var(--tw-shadow-color)] shadow-[#896FBC]">real </span>
+          <span className="font-black shadow-purple-400">businesses and non-profits</span>
+        </div>
+        <div className="w-10/12 font-semibold text-lg">
+          Many lack the resources to build high-quality custom tech solutions to meet their needs. That’s where you come in. At Code for a Cause, you’ll be creating solutions that help those that need it most. 
+        </div>
+      </div>
+      <div className="p-2 w-5/12 ml-auto mr-auto text-center justify-contents-center font-bold text-lg">
+        What a project might look like
+        <div className="w-fit ml-auto mr-auto mt-2 bg-[#D9D9D9] rounded-[16px] p-8 tracking-wide text-black font-bold space-y-2 italic">
+          <div className="line-through decoration-red-500 decoration-2">solve food wastage for the 1000th time</div>
+          <div className="">help the local Goodwill automate pickups</div>
+        </div>
+        <div className="text-[10px] font-normal">
+          Note: this is just an example! we’ll have 10-15 different tracks like this to choose from!
+        </div>
+      </div>
+      
+    </div>
+  )
+} 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,13 @@ module.exports = {
 		"./src/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			textShadow: {
+				sm: "0 1px 2px var(--tw-shadow-color)",
+				DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+				lg: "0 8px 16px var(--tw-shadow-color)",
+			},
+		},
 	},
 	plugins: [],
 };
